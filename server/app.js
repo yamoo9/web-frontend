57818555f49978c41dcd763c44d95f8950406ae6
yamoo9 <yamoo9@naver.com>
2022-12-15 13:38:47 +0900

    learn. Express 정적 호스팅(서빙)

--- server/app.js
+++ server/app.js
@@ -1,29 +1,13 @@
-// ES 모듈 vs. CommonJS 모듈
-// import '모듈경로'    | require('모듈경로')
-// export default 모듈 | module.exports = 모듈
-// export 모듈         | exports.모듈
-
+const { resolve } = require('node:path');
 const express = require('express');
 const app = express();
 const PORT = 4000;
 
-app.get('/', (req, res) => {
-  res.send(/* html */ `
-    <!DOCTYPE html>
-      <html lang="ko-KR">
-      <head>
-        <meta charset="UTF-8" />
-        <title>안녕 Express 😃</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </head>
-      <body>
-        <div class="container">
-          <h1>Express 서버 애플리케이션</h1>
-        </div>
-      </body>
-    </html>
-  `);
-});
+// /Users/yamoo9/Documents/PROJECT/web-frontend/client/public
+const PUBLIC_DIR = resolve(__dirname, '../client/public');
+
+// app.use(middleware_function)
+app.use(express.static(PUBLIC_DIR));
 
 app.listen(PORT, () => {
   console.log(`Express 서버 구동: http://localhost:${PORT}`);
